a7ed451a3daaafdc46784eb5fe678b2fa782e391
Misfortunia <misfortunian@gmail.com>
2025-03-25 21:29:26 +0700

    Infinicky - Update 2

--- src/main/java/com/example/examplemod/block/nickyblock.java
+++ src/main/java/com/example/examplemod/block/nickyblock.java
@@ -15,14 +15,19 @@ public class nickyblock {
     //Creating DeferredRegister.Blocks
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks("guinnevere");
 
-    //Registering the block "Infinicky"
-    public static final DeferredBlock<Block> INFINICKY = BLOCKS.register("infinicky",
+    public static final DeferredBlock<Block> INFINICKY = BLOCKS.register("infinicky", //Registering the block "Infinicky"
     //FUNCTIONS OF INFINICKY- SIMPLE - MIGHT WANT TO CHANGE THIS
             registryName -> new Block(BlockBehaviour.Properties.of()
                     .setId(ResourceKey.create(Registries.BLOCK, registryName)) //Sets ID of the block
                     .destroyTime(5.0f) // Sets the destroy time
-                    .explosionResistance(10.0f)
-                    .sound(SoundType.AMETHYST)
-                    .lightLevel(state -> 5)
-            ));
+                    .explosionResistance(10.0f) // Sets the explosion resistance
+                    .sound(SoundType.AMETHYST) // Sets the soundtype into [Amethyst]
+                    .lightLevel(state -> 5) // Sets the lightlevel to 5
+            )
+    );
+
+//    public static final DeferredBlock<Block> INFINICKY = BLOCKS.registerBlock("infinicky",
+//            Block::new,
+//            BlockBehaviour.Properties.of()
+//    );
 }
